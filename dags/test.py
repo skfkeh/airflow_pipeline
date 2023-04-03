32755a11f3b521265c06ed9bd6244ff44b44cc7f
skfkeh <>
2023-04-03 16:53:27 +0900

    first commit

---
+++ dags/test.py
@@ -0,0 +1,40 @@
+from datetime import datetime, timedelta
+import requests
+from bs4 import BeautifulSoup
+import mysql.connector
+from airflow import DAG
+
+url = 'https://finance.naver.com/sise/lastsearch2.nhn'
+response = requests.get(url)
+html = response.text
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find('table', {'class': 'type_5'})
+rows = table.tbody.find_all('tr')
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="1234",
+    database="crawling_data"
+)
+
+mycursor = mydb.cursor()
+
+# Create a new table for the stock price data
+mycursor.execute("CREATE TABLE IF NOT EXISTS korea_top1000_stocks (date DATETIME, rank INT, company VARCHAR(255), price INT)")
+
+for row in rows:
+    cols = row.find_all('td')
+    if len(cols) > 1:
+        rank = cols[0].text.strip()
+        company = cols[1].text.strip()
+        price = cols[2].text.strip().replace(",", "")
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        stock_list.append(company)
+
+        sql = "INSERT INTO korea_top1000_stocks (date, rank, company, price) VALUES (%s, %s, %s, %s)"
+        val = (date, rank, company, price)
+
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print(mycursor.rowcount, "record inserted.")
